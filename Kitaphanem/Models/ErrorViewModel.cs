53dcf256b1eb71b1c83caaa1014e7d53b4cd9517
Tubaasvise <Kullanc@QUINNZY>
2025-04-17 02:15:52 +0300

    hebele

---
+++ Kitaphanem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kitaphanem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
